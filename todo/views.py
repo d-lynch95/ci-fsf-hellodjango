e20d5e93d6d870dcbb044b26fade379da87f5bfc
d-lynch95 <>
2023-07-19 01:28:49 +0000

    add edit items file

--- todo/views.py
+++ todo/views.py
@@ -15,9 +15,21 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
         name = request.POST.get("item_name")
         done = 'done' in request.POST
         Item.objects.create(name=name, done=done)
 
         return redirect('get_todo_list')
-    return render(request, 'todo/add_item.html')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    return render(request, 'todo/edit_item.html')
